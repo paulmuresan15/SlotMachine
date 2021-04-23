24c425bd0a2aeda810bcc012c032018a20e7be18
Muresan Paul Mihai <Muresan.Paul@student.utcluj.ro>
2021-04-23 16:37:31 +0300

    Added readme

--- src/main/java/slotmachine/GUI.java
+++ src/main/java/slotmachine/GUI.java
@@ -35,185 +35,43 @@ public class GUI {
         return betTextField;
     }
 
-    public void setBetTextField(JTextField betTextField) {
-        this.betTextField = betTextField;
-    }
+
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
 
-    public void setMainPanel(JPanel mainPanel) {
-        this.mainPanel = mainPanel;
-    }
+
 
     public JButton getPlayButton() {
         return playButton;
     }
 
-    public void setPlayButton(JButton playButton) {
-        this.playButton = playButton;
-    }
+
 
     public JTextField getScoreTextField() {
         return scoreTextField;
     }
 
-    public void setScoreTextField(JTextField scoreTextField) {
-        this.scoreTextField = scoreTextField;
-    }
 
     public JTextField getCreditTextField() {
         return creditTextField;
-    }
-
-    public void setCreditTextField(JTextField creditTextField) {
-        this.creditTextField = creditTextField;
     }
 
     public JTextField getAddCreditTextField() {
         return addCreditTextField;
     }
 
-    public void setAddCreditTextField(JTextField addCreditTextField) {
-        this.addCreditTextField = addCreditTextField;
-    }
 
     public JButton getAddMoneyButton() {
         return addMoneyButton;
     }
 
-    public void setAddMoneyButton(JButton addMoneyButton) {
-        this.addMoneyButton = addMoneyButton;
-    }
 
-    public JTextField getTextField1() {
-        return betTextField;
-    }
 
-    public void setTextField1(JTextField textField1) {
-        this.betTextField = textField1;
-    }
 
-    public JLabel getImg11() {
-        return img11;
-    }
 
-    public void setImg11(JLabel img11) {
-        this.img11 = img11;
-    }
 
-    public JLabel getImg12() {
-        return img12;
-    }
-
-    public void setImg12(JLabel img12) {
-        this.img12 = img12;
-    }
-
-    public JLabel getImg13() {
-        return img13;
-    }
-
-    public void setImg13(JLabel img13) {
-        this.img13 = img13;
-    }
-
-    public JLabel getImg14() {
-        return img14;
-    }
-
-    public void setImg14(JLabel img14) {
-        this.img14 = img14;
-    }
-
-    public JLabel getImg15() {
-        return img15;
-    }
-
-    public void setImg15(JLabel img15) {
-        this.img15 = img15;
-    }
-
-    public JLabel getImg21() {
-        return img21;
-    }
-
-    public void setImg21(JLabel img21) {
-        this.img21 = img21;
-    }
-
-    public JLabel getImg22() {
-        return img22;
-    }
-
-    public void setImg22(JLabel img22) {
-        this.img22 = img22;
-    }
-
-    public JLabel getImg23() {
-        return img23;
-    }
-
-    public void setImg23(JLabel img23) {
-        this.img23 = img23;
-    }
-
-    public JLabel getImg24() {
-        return img24;
-    }
-
-    public void setImg24(JLabel img24) {
-        this.img24 = img24;
-    }
-
-    public JLabel getImg25() {
-        return img25;
-    }
-
-    public void setImg25(JLabel img25) {
-        this.img25 = img25;
-    }
-
-    public JLabel getImg31() {
-        return img31;
-    }
-
-    public void setImg31(JLabel img31) {
-        this.img31 = img31;
-    }
-
-    public JLabel getImg32() {
-        return img32;
-    }
-
-    public void setImg32(JLabel img32) {
-        this.img32 = img32;
-    }
-
-    public JLabel getImg33() {
-        return img33;
-    }
-
-    public void setImg33(JLabel img33) {
-        this.img33 = img33;
-    }
-
-    public JLabel getImg34() {
-        return img34;
-    }
-
-    public void setImg34(JLabel img34) {
-        this.img34 = img34;
-    }
-
-    public JLabel getImg35() {
-        return img35;
-    }
-
-    public void setImg35(JLabel img35) {
-        this.img35 = img35;
-    }
 
     public JPanel getPanelPhotos() {
         return panelPhotos;
@@ -223,6 +81,7 @@ public class GUI {
         JFrame frame=new JFrame(title);
         frame.add(mainPanel);
         frame.setSize(880,550);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
